4af78dcc382410fc4bca55ab0a93761ca7b5820c
Håvard Pettersen <h@havpet.no>
2016-11-11 12:50:34 +0100

    Some template code

--- MobileAgent.java
+++ MobileAgent.java
@@ -1,5 +1,20 @@
 package no.hib.mod252.oblig3;
 
-public class MobileAgent {
+import jade.core.*;
+
+public class MobileAgent extends Agent {
+	
+	public void setup() {		
+		AID remote = new AID("ams@127.0.0.1:1099/JADE", AID.ISGUID);
+		remote.addAddresses("http://GeekWin:7778/acc");
+		
+		PlatformID destination = new PlatformID(remote);
+		
+		doMove(destination);
+		
+		
+	}
+	
+	
 
 }
