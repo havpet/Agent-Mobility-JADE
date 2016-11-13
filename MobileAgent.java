d0a95ec27292459713bacafe7c54172820b61fd5
Håvard Pettersen <h@havpet.no>
2016-11-13 16:30:36 +0100

    Agent is now functional and is using RAM as measure

--- MobileAgent.java
+++ MobileAgent.java
@@ -1,19 +1,86 @@
 package no.hib.mod252.oblig3;
 
 import jade.core.*;
+import jade.core.behaviours.CyclicBehaviour;
+import java.lang.Runtime;
 
 public class MobileAgent extends Agent {
 	
-	public void setup() {		
-		AID remote = new AID("ams@127.0.0.1:1099/JADE", AID.ISGUID);
-		remote.addAddresses("http://GeekWin:7778/acc");
-		
+	private int _state = 0;
+	private int mostAvailable = -1;
+	private long availableResources = 0;
+	private int location = 0;
+	private boolean taskPerformed = false;
+	private PlatformID[] _dests;
+	
+	public void setup() {	
+		_dests = new PlatformID[2];
+		AID remote = new AID("amm@Laptop", AID.ISGUID);
+		remote.addAddresses("http://DESKTOP-L0V7CP0:7778/acc");
 		PlatformID destination = new PlatformID(remote);
+		_dests[0] = destination;
 		
-		doMove(destination);
+		AID remote2 = new AID("amm@Desktop", AID.ISGUID);
+		remote2.addAddresses("http://GeekWin:7778/acc");
+		PlatformID destination2 =  new PlatformID(remote2);
+		_dests[1] = destination2;
 		
+		addBehaviour(new CyclicBehaviour(this) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void action() {
+				switch(_state) {
+				case 0:
+					_state++;
+					location = 0;
+					System.out.println("**Agent is here**");
+					if(Runtime.getRuntime().freeMemory() > availableResources) {
+						availableResources = Runtime.getRuntime().freeMemory();
+						mostAvailable = location;
+					}
+					System.out.println("Moving to " + _dests[0].getName() + "..");
+					myAgent.doMove(_dests[0]);
+					break;
+				case 1:
+					_state++;
+					location = 1;
+					System.out.println("**Agent is here**");
+					if(Runtime.getRuntime().freeMemory() > availableResources) {
+						availableResources = Runtime.getRuntime().freeMemory();
+						mostAvailable = location;
+					}
+					System.out.println("Moving to " + _dests[1].getName() + "..");
+					myAgent.doMove(_dests[1]);
+					break;
+				case 2:
+					if(location != mostAvailable) {
+						myAgent.doMove(_dests[mostAvailable]);
+					}
+					else {
+						taskPerformed = true;
+						System.out.println("**Agent is here**");
+						System.out.println("Performing task..");
+					}
+				default:
+					if(!taskPerformed) {
+						taskPerformed = true;
+						System.out.println("**Agent is here**");
+						System.out.println("Performing task..");
+					}
+					myAgent.doDelete();
+				}
+			}
+			
+		});
 		
 	}
+	
+	public void beforeMove() {
+	}
+	
+	public void afterMove() {
+    }
 	
 	
 
