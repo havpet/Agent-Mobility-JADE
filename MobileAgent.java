6b298802e4289ba6014878a7d9b5f5953cf81ba8
Håvard Pettersen <h@havpet.no>
2016-11-13 19:31:30 +0100

    Now works with 3 computers

--- MobileAgent.java
+++ MobileAgent.java
@@ -14,7 +14,7 @@ public class MobileAgent extends Agent {
 	private PlatformID[] _dests;
 	
 	public void setup() {	
-		_dests = new PlatformID[2];
+		_dests = new PlatformID[3];
 		AID remote = new AID("amm@Laptop", AID.ISGUID);
 		remote.addAddresses("http://DESKTOP-L0V7CP0:7778/acc");
 		PlatformID destination = new PlatformID(remote);
@@ -25,47 +25,87 @@ public class MobileAgent extends Agent {
 		PlatformID destination2 =  new PlatformID(remote2);
 		_dests[1] = destination2;
 		
+		AID remote3 = new AID("amm@Laptop2", AID.ISGUID);
+		remote3.addAddresses("http://LAPTOP-93M2G80U:7778/acc");
+		PlatformID destination3 =  new PlatformID(remote3);
+		_dests[2] = destination3;
+		
+		System.out.println("Creating agent");
+		
 		addBehaviour(new CyclicBehaviour(this) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void action() {
 				switch(_state) {
+				
 				case 0:
 					_state++;
-					location = 0;
-					System.out.println("**Agent is here**");
-					if(Runtime.getRuntime().freeMemory() > availableResources) {
-						availableResources = Runtime.getRuntime().freeMemory();
-						mostAvailable = location;
-					}
-					System.out.println("Moving to " + _dests[0].getName() + "..");
-					myAgent.doMove(_dests[0]);
+					System.out.println("Agent started");
+					System.out.println("Moving to " + _dests[location].getName() + "..");
+					myAgent.doMove(_dests[location]);
 					break;
+					
 				case 1:
 					_state++;
-					location = 1;
-					System.out.println("**Agent is here**");
+					System.out.println(">>Agent is here<<");
 					if(Runtime.getRuntime().freeMemory() > availableResources) {
 						availableResources = Runtime.getRuntime().freeMemory();
 						mostAvailable = location;
 					}
-					System.out.println("Moving to " + _dests[1].getName() + "..");
-					myAgent.doMove(_dests[1]);
+					location++;
+					if(location < _dests.length) {	
+						System.out.println("Moving to " + _dests[location].getName() + "..");
+						myAgent.doMove(_dests[location]);
+					}
 					break;
+					
 				case 2:
+					_state++;
+					System.out.println(">>Agent is here<<");
+					if(Runtime.getRuntime().freeMemory() > availableResources) {
+						availableResources = Runtime.getRuntime().freeMemory();
+						mostAvailable = location;
+					}
+					location++;
+					if(location < _dests.length) {	
+						System.out.println("Moving to " + _dests[location].getName() + "..");
+						myAgent.doMove(_dests[location]);
+					}
+					break;
+					
+				case 3:
+					_state++;
+					System.out.println(">>Agent is here<<");
+					if(Runtime.getRuntime().freeMemory() > availableResources) {
+						availableResources = Runtime.getRuntime().freeMemory();
+						mostAvailable = location;
+					}
+					location++;
+					if(location < _dests.length) {	
+						System.out.println("Moving to " + _dests[location].getName() + "..");
+						myAgent.doMove(_dests[location]);
+					}
+					break;
+					
+				case 4:
+					_state++;
+					System.out.println(">>Agent is here<<");
 					if(location != mostAvailable) {
+						System.out.println("Moving to " + _dests[mostAvailable].getName() + " because it won..");
 						myAgent.doMove(_dests[mostAvailable]);
 					}
 					else {
 						taskPerformed = true;
-						System.out.println("**Agent is here**");
 						System.out.println("Performing task..");
 					}
+					break;
+					
 				default:
+					_state++;
 					if(!taskPerformed) {
 						taskPerformed = true;
-						System.out.println("**Agent is here**");
+						System.out.println(">>Agent is here<<");
 						System.out.println("Performing task..");
 					}
 					myAgent.doDelete();
